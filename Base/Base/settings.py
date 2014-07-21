addfc48e0b497cc433a7ba71abbdd074fdafd270
HammadH <hammadsyed9@gmail.com>
2014-07-21 14:29:18 +0400

    tweaks

--- Base/Base/settings.py
+++ Base/Base/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = 'kc7@538vl4ip+_72@34-zlgf60@xs@uxf43)dy%v@bb^@w08u@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 TEMPLATE_DEBUG = True
 

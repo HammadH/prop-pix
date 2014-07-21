83e969e6ca86a802a3aecf6216f5bb39e4a691b4
HammadH <hammadsyed9@gmail.com>
2014-07-21 16:33:47 +0400

    ajax

--- Base/views.py
+++ Base/views.py
@@ -1,5 +1,7 @@
+import json
+
 from django.views.generic import TemplateView
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse
 
 from emails.models import Leads
 
@@ -7,12 +9,19 @@ class IndexView(TemplateView):
 	template_name = 'index.html'
 
 	def post(self, request, *args, **kwargs):
-		email = request.POST.get('email')
-		try:
-			new_lead = Leads.objects.create(email=email)
-		except:
-			pass
-		return HttpResponseRedirect('/') 
+		if request.is_ajax():
+			email = request.POST.get('email')
+			response_data = {}
+			try:
+				new_lead = Leads.objects.create(email=email)
+				response_data['status'] = 'success'
+				response_data['message'] = 'Thank you for your interest! We shall contact you very soon.'
+			except:
+				response_data['status'] = 'failed'
+				response_data['message'] = "Oops! Your email didn't go through! Try again?"
+				
+			
+			return  HttpResponse(json.dumps(response_data), content_type="application/json")
 
 
 home = IndexView.as_view()
